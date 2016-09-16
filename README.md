66ff0d5ed6215eea0a91a504a9a6bdf7e21374b9
Pradip Hudekar <phudekar@equalexperts.com>
2016-09-17 00:35:31 +0530

    basic cf schema

---
+++ README.md
@@ -0,0 +1,17 @@
+Cloud Formation Template JSON Schema
+====
+
+This repository provide customized Json schema for providing validation and code completion for AWS Cloud Formation templates.
+The schema `cfschema.json` is located under `.vscode` directory
+
+## Using the schema
+
+Once you open the repository in VSCode editor. It will provide you code completion and validation for Cloud Formation templates files which has `.cf.json` extenstion.
+
+### Currently following Resources are supported
+
+- AWS::EC2::DHCPOptions
+
+- AWS::EC2::EIP
+                
+- AWS::EC2::VPC
---
+++ scrapper.js
@@ -0,0 +1,54 @@
+var program = $(".programlisting");
+var str = "";
+if (program) {
+    if (program[0])
+        str = program[0].innerText;
+    else
+        str = program.innerText;
+}
+
+var getArray = function (t) {
+    return {
+        "type": "array",
+        "items": getType(t)
+    };
+}
+
+var getType = function (t) {
+    return {
+        "type": t
+    };
+}
+
+var stringKeys = ["String", "Resource Tag", "Stage Key"];
+
+for (key in stringKeys) {
+    var regExp = new RegExp('\\[ ' + stringKeys[key] + ', ... ]', 'g');
+    str = str.replace(regExp, JSON.stringify(getArray("string")));
+}
+
+str = str.replace(/\[ Boolean, ... ]/g, JSON.stringify(getArray("boolean")));
+str = str.replace(/Boolean/g, JSON.stringify(getType("boolean")));
+
+str = str.replace(/\[ Number, ... ]/g, JSON.stringify(getArray("number")));
+str = str.replace(/Number/g, JSON.stringify(getType("number")));
+
+str = str.replace(/String/g, JSON.stringify(getType("string")));
+
+var obj = JSON.parse(str);
+
+var item = {
+    "type": "object",
+    "properties": {
+        "Type": {
+            "$ref": "#/definitions/resourceType",
+            "pattern": obj.Type,
+        },
+        "Properties": {
+            "type": "object",
+            "properties": obj.Properties
+        }
+    }
+};
+
+console.log(JSON.stringify(item));
